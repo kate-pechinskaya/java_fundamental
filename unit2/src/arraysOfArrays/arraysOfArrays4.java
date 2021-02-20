9745c86cea69a7353f47c6e22e588be4f2e4f8bd
kate-pechinskaya <katttka98@gmail.com>
2021-02-20 17:19:18 +0300

    edds text for arraysOfArrays 4-7

--- unit2/src/arraysOfArrays/arraysOfArrays4.java
+++ unit2/src/arraysOfArrays/arraysOfArrays4.java
@@ -1,7 +1,16 @@
 package arraysOfArrays;
 public class arraysOfArrays4 {
     /*4.	Сформировать квадратную матрицу порядка n
-     по заданному образцу(n - четное). */
+     по заданному образцу(n - четное).
+      1   2    3  ... n
+      n  n-1  n-2 ... 1
+      1   2    3  ... n
+      n  n-1  n-2 ... 1
+      .   .     . ... .
+      .   .     . ... .
+      .   .     . ... .
+      n  n-1  n-2 ... 1
+     */
     public static void main(String[] args) {
 
     }
--- unit2/src/arraysOfArrays/arraysOfArrays7.java
+++ unit2/src/arraysOfArrays/arraysOfArrays7.java
@@ -1,7 +1,9 @@
 package arraysOfArrays;
 public class arraysOfArrays7 {
     /*7.	Сформировать квадратную матрицу порядка N по правилу:
-    и подсчитать количество положительных элементов в ней. */
+    и подсчитать количество положительных элементов в ней.
+    A[I,J] = sin((I^2 - J^2)/N)
+    */
     public static void main(String[] args) {
 
     }
