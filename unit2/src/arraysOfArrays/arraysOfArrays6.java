401759e9218f2a2f5d0087b65dd272169b3f0300
kate-pechinskaya <katttka98@gmail.com>
2021-02-25 13:48:22 +0300

    arraysOfArrays6 done

--- unit2/src/arraysOfArrays/arraysOfArrays6.java
+++ unit2/src/arraysOfArrays/arraysOfArrays6.java
@@ -14,20 +14,17 @@ public class arraysOfArrays6 {
     public static void main(String[] args) {
         int[][] arr = new int[5][5];
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if(i+j < arr.length)
-                    arr[i][j] = 1;
-                else arr[i][j] = 0;
-
+        for (int i = 0; i < arr.length /2 + 1 ; i++) {
+            for (int j =  i; j < arr.length - i; j++) {
+                arr[i][j] = 1;
+                arr[arr.length - i - 1][j] = 1;
             }
         }
-        //вывод результата
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                System.out.print(arr[i][j] + " ");
+        for (int i = 0; i< arr.length; i++){
+            for (int j = 0; j< arr.length; j++){
+                System.out.print(" " + arr[i][j] + " ");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
