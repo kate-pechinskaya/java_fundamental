138ceb070cff6cc5ea0801aeb24f8ab86f536cf6
kate-pechinskaya <katttka98@gmail.com>
2021-03-02 14:27:32 +0300

    adds arraysOfArrays11

--- unit2/src/arraysOfArrays/arraysOfArrays11.java
+++ unit2/src/arraysOfArrays/arraysOfArrays11.java
@@ -4,6 +4,25 @@ public class arraysOfArrays11 {
      Вывести на экран саму матрицу и номера строк, в которых число
       5 встречается три и более раз.*/
     public static void main(String[] args) {
-
+        int[][] a = new int[10][20];
+        for (int i = 0; i < 10; i++) {
+            int count =0;
+            for (int j = 0; j < 20; j++) {
+                a[i][j] = (int) (Math.random() * 15);
+                if (a[i][j] == 5) {
+                    count++;
+                }
+                if (count >= 3) {
+                    System.out.println("Строка :" + i);
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 20; j++) {
+                System.out.print(a[i][j]+ " ");
+            }
+            System.out.println();
+        }
     }
 }
