e0d174274d1bd1722fbeccb0cc79a1c9c94a007d
kate-pechinskaya <katttka98@gmail.com>
2021-03-26 10:46:05 +0300

    adds finish version decompositon 13

--- unit2/src/decomposition/decomposition13.java
+++ unit2/src/decomposition/decomposition13.java
@@ -19,7 +19,7 @@ public class decomposition13 {
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         array(20);
     }
 }
